eb8c31cac2444d04a1876d5f29bbc0f76b58c8f3
pryanik <Yan.Kovalenko@Tune-IT.Ru>
2026-05-25 23:38:19 +0200

    Fix asset lookup in production

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,11 @@
 module ImagesHelper
+  def image_source_for(image)
+    if image.photo.attached?
+      url_for(image.photo)
+    elsif image.file_name.present?
+      asset_path("pictures/#{image.file_name}")
+    end
+  rescue Sprockets::Rails::Helper::AssetNotFound
+    nil
+  end
 end
